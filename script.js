0d6609e960f46768aca3be31be027cd69a769c35
unknown <nurmansyahbachtyar@gmail.com>
2023-11-08 03:31:03 +0700

    update 8 nov

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ function scrollToElement(elementSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
@@ -22,5 +24,11 @@ link2.addEventListener('click', () => {
 });
 
 link3.addEventListener('click', () => {
-    scrollToElement('.column');
+    scrollToElement('.header', 2);
+});
+link4.addEventListener('click', () => {
+    scrollToElement('.header', 3);
+});
+link5.addEventListener('click', () => {
+    scrollToElement('.header', 4);
 });
