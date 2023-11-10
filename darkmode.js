6a9128e9f755236cb1820435fa2e82c4eced8688
unknown <nurmansyahbachtyar@gmail.com>
2023-11-11 02:46:27 +0700

    update 11 nov darkmode

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,16 @@
 let body = document.body;
 let mode = document.getElementById("mode");
+// let img = document.getElementById("img");
 
 function darkMode(){
     // console.log('darkmode di klik');
     body.classList.toggle("dark");
-    mode.classList.toggle("ri-sun-fill");
+    if (body.classList.contains("dark")){
+        document.getElementById("img").src="images/logo-light.png";
+        document.getElementById("img-footer").src="images/logo-light.png";
+    } else {
+        document.getElementById("img").src="images/logo.png";
+        document.getElementById("img-footer").src="images/logo.png";
+    }
+    // mode.classList.toggle("ri-sun-fill");
 }
